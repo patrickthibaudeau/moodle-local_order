ff2c973cc8f53fc2b55a81505928f0f6cdbae5c3
Patrick Thibaudeau <thibaud@yorku.ca>
2023-04-16 23:13:09 -0400

    Added the ability to only export based on the inventory category type

--- export/excel.php
+++ export/excel.php
@@ -18,12 +18,26 @@ $building = optional_param('building', '', PARAM_TEXT);
 $room = optional_param('room', '', PARAM_TEXT);
 $status = optional_param('status', -1, PARAM_INT);
 $organization = optional_param('organization', -1, PARAM_INT);
-$filename = 'data_export.csv';
-$columns = set_columns();
+switch ($inventory_category_id) {
+    case 1:
+        $filename = 'congress_' . date('Y', time()) . '_export_av.csv';
+        break;
+    case 2:
+        $filename = 'congress_' . date('Y', time()) . '_export_catering.csv';
+        break;
+    case 3:
+        $filename = 'congress_' . date('Y', time()) . '_export_furnishing.csv';
+        break;
+    default:
+        $filename = 'congress_' . date('Y', time()) . 'all_export.csv';
+        break;
+}
+
+$columns = set_columns($inventory_category_id);
 // tell the browser it's going to be a csv file
 header('Content-Type: text/csv');
 // tell the browser we want to save it instead of displaying it
-header('Content-Disposition: attachment; filename="'.$filename.'";');
+header('Content-Disposition: attachment; filename="' . $filename . '";');
 // Open a file for writing
 
 $fp = fopen('php://output', 'w');
@@ -36,7 +50,7 @@ if ($id) {
     // Get event data
     $data = $EVENT->get_data_for_pdf($inventory_category_id);
     // Prepare data for CSV row
-    $event_data = prepare_data_single_event($data, $columns);
+    $event_data = prepare_data_single_event($data, $columns, $inventory_category_id);
 // Write each row of data to the file
     fputcsv($fp, $event_data);
     unset($EVENT);
@@ -52,7 +66,7 @@ if ($id) {
         // Get event data
         $data = $EVENT->get_data_for_pdf($inventory_category_id);
         // Prepare data for CSV row
-        $event_data = prepare_data_single_event($data, $columns);
+        $event_data = prepare_data_single_event($data, $columns, $inventory_category_id);
         fputcsv($fp, $event_data);
         unset($EVENT);
     }
@@ -61,14 +75,20 @@ if ($id) {
 // Close the file
 fclose($fp);
 
-function set_columns()
+function set_columns($inventory_category_id = 0)
 {
     global $DB;
 
 // Get all inventory items as columns by category
-    $av_inventory = $DB->get_records('order_inventory', ['inventorycategoryid' => 1]);
-    $catering_inventory = $DB->get_records('order_inventory', ['inventorycategoryid' => 2], 'name DESC');
-    $furnishing_inventory = $DB->get_records('order_inventory', ['inventorycategoryid' => 3]);
+    if ($inventory_category_id == 0 || $inventory_category_id == 1) {
+        $av_inventory = $DB->get_records('order_inventory', ['inventorycategoryid' => 1]);
+    }
+    if ($inventory_category_id == 0 || $inventory_category_id == 2) {
+        $catering_inventory = $DB->get_records('order_inventory', ['inventorycategoryid' => 2], 'name DESC');
+    }
+    if ($inventory_category_id == 0 || $inventory_category_id == 3) {
+        $furnishing_inventory = $DB->get_records('order_inventory', ['inventorycategoryid' => 3]);
+    }
 
     $columns = array();
     $columns[] = 'registration_id';
@@ -82,45 +102,70 @@ function set_columns()
     $columns[] = 'setup_notes';
     $columns[] = 'other_notes';
     // Furnishing
-    foreach ($furnishing_inventory as $fi) {
-        $columns[] = $fi->name;
-    }
-    // AV
-    foreach ($av_inventory as $ai) {
-        $columns[] = $ai->name;
-    }
-    // Catering
-    // For catering we must repeat for each meal
-    // Posisble numebr of meals is 4
-    // Meal 1
-    foreach ($catering_inventory as $ci) {
-        $columns[] = $ci->name;
-    }
-    // Meal 2
-    foreach ($catering_inventory as $ci) {
-        $columns[] = str_replace('1-', '2-', $ci->name);
-    }
-    // Meal 3
-    foreach ($catering_inventory as $ci) {
-        $columns[] = str_replace('1-', '3-', $ci->name);
-    }
-    // Meal 4
-    foreach ($catering_inventory as $ci) {
-        $columns[] = str_replace('1-', '4-', $ci->name);
+    if ($inventory_category_id == 0 || $inventory_category_id == 3) {
+        foreach ($furnishing_inventory as $fi) {
+            $columns[] = $fi->name;
+        }
     }
 
-    $columns[] = 'cost';
-    $columns[] = 'chargebackaccount';
+    // AV
+    if ($inventory_category_id == 0 || $inventory_category_id == 1) {
+        foreach ($av_inventory as $ai) {
+            $columns[] = $ai->name;
+        }
+    }
+    // Catering
+    if ($inventory_category_id == 0 || $inventory_category_id == 2) {
+        // For catering we must repeat for each meal
+        // Posisble numebr of meals is 4
+        // Meal 1
+        foreach ($catering_inventory as $ci) {
+            $columns[] = $ci->name;
+        }
+        // Meal 2
+        foreach ($catering_inventory as $ci) {
+            $columns[] = str_replace('1-', '2-', $ci->name);
+        }
+        // Meal 3
+        foreach ($catering_inventory as $ci) {
+            $columns[] = str_replace('1-', '3-', $ci->name);
+        }
+        // Meal 4
+        foreach ($catering_inventory as $ci) {
+            $columns[] = str_replace('1-', '4-', $ci->name);
+        }
+    }
+
+    if ($inventory_category_id == 0) {
+        $columns[] = 'cost';
+        $columns[] = 'chargebackaccount';
+    }
+
     return $columns;
 }
 
-function prepare_data_single_event($event_data, $columns)
+function prepare_data_single_event($event_data, $columns, $inventory_category_id = 0)
 {
     $data = [];
 
-    $furnishing = @$event_data->inventory_items[2]->items[0]['items'];
-    $av = @$event_data->inventory_items[0]->items[0]['items'];
-    $catering = @$event_data->inventory_items[1]->items;
+    if ($inventory_category_id == 0 || $inventory_category_id == 1) {
+        $av = @$event_data->inventory_items[0]->items[0]['items'];
+    }
+    if ($inventory_category_id == 0 || $inventory_category_id == 2) {
+        if ($inventory_category_id == 2) {
+            $catering = @$event_data->inventory_items[0]->items;
+        } else {
+            $catering = @$event_data->inventory_items[1]->items;
+        }
+    }
+    if ($inventory_category_id == 0 || $inventory_category_id == 3) {
+        if ($inventory_category_id == 3) {
+            $furnishing = @$event_data->inventory_items[0]->items[0]['items'];
+        } else {
+            $furnishing = @$event_data->inventory_items[2]->items[0]['items'];
+        }
+
+    }
 
 //print_object($event_data);
     $data[] = $event_data->code;
@@ -140,38 +185,48 @@ function prepare_data_single_event($event_data, $columns)
         }
     }
 
-    $data[429] = $event_data->cost;
-    $data[430] = $event_data->organization->costcentre . '-'
-        . $event_data->organization->fund . '-'
-        . $event_data->organization->activitycode;
+    if ($inventory_category_id == 0) {
+        $data[429] = $event_data->cost;
+        $data[430] = $event_data->organization->costcentre . '-'
+            . $event_data->organization->fund . '-'
+            . $event_data->organization->activitycode;
+    }
+
 
     // Get furnishing Data
-    if (isset($furnishing)) {
-        foreach ($furnishing as $f) {
-            $key = array_search(trim($f->name), $columns);
-            $data[$key] = trim($f->quantity . ' ' . $f->description);
+    if ($inventory_category_id == 0 || $inventory_category_id == 3) {
+        if (isset($furnishing)) {
+            foreach ($furnishing as $f) {
+                $key = array_search(trim($f->name), $columns);
+                $data[$key] = trim($f->quantity . ' ' . $f->description);
+            }
         }
     }
 
 
     //Get AV Data
-    if (isset($av)) {
-        foreach ($av as $a) {
-            $key = array_search(trim($a->name), $columns);
-            $data[$key] = trim($a->quantity . ' ' . $a->description);
-        }
-    }
-
-    if (isset($catering)) {
-        foreach ($catering as $section_key => $section) {
-            $section_key = $section_key + 1;
-            $items = $section['items'];
-            foreach ($items as $item_key => $c) {
-                $name = str_replace('1-', $section_key . '-', $c->name);
-                $key = array_search(trim($name), $columns);
-                $data[$key] = trim(str_replace('1 ', '', $c->quantity . ' ' . $c->description));
+    if ($inventory_category_id == 0 || $inventory_category_id == 1) {
+        if (isset($av)) {
+            foreach ($av as $a) {
+                $key = array_search(trim($a->name), $columns);
+                $data[$key] = trim($a->quantity . ' ' . $a->description);
             }
         }
     }
+
+    if ($inventory_category_id == 0 || $inventory_category_id == 2) {
+        if (isset($catering)) {
+            foreach ($catering as $section_key => $section) {
+                $section_key = $section_key + 1;
+                $items = $section['items'];
+                foreach ($items as $item_key => $c) {
+                    $name = str_replace('1-', $section_key . '-', $c->name);
+                    $key = array_search(trim($name), $columns);
+                    $data[$key] = trim(str_replace('1 ', '', $c->quantity . ' ' . $c->description));
+                }
+            }
+        }
+    }
+
     return $data;
 }
